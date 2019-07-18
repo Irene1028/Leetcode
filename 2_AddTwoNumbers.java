0b4a0a743daf42cf85b189452259b2c005c5beeb
Irene1028 <49041696+Irene1028@users.noreply.github.com>
2019-07-18 17:56:17 -0400

    Update and rename AddTwoNumbers.java to 2_AddTwoNumbers.java

--- 2_AddTwoNumbers.java
+++ 2_AddTwoNumbers.java
@@ -1,15 +1,15 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
+
+//Definition for singly-linked list.
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
  
- idea: We have three numbers to add: node1.val, node2.val, carry
+ 
+ /*idea: We have three numbers to add: node1.val, node2.val, carry
  Consider carry's value in while loop will decrease run time.
- We do not have to use an extra "if" for the highest digit.
+ We do not have to use an extra "if" for the highest digit.*/
  
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
