3ac610ad9cdf1e12d6cb7fa724adb945a5ec157b
Irene1028 <49041696+Irene1028@users.noreply.github.com>
2019-07-21 16:49:30 -0400

    Create 88_MergeSortedArray.java

---
+++ Array/88_MergeSortedArray.java
@@ -0,0 +1,22 @@
+// a nice solution
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int k = m + n - 1;
+        int i = m - 1, j = n - 1;
+        while(i>-1 && j>-1){
+            nums1[k--] = (nums1[i]>nums2[j])? nums1[i--]: nums2[j--];
+        }
+        // The second while is what confuses me.
+        // Why we consider j>-1, rather than i>-1?
+        // When we jumped out from the first loop, there are two conditions:
+        // 1. nums2 run out     2. nums1 run out
+        // in case 1, we are sure all the nums2 elements were put in appropriate positions.
+        // So the left nums1 elements are where they should be, nothing we need to do. Just return.
+        // Thus, we only consider consition2. We should copy left numbers to nums1.
+        while(j>-1){
+            nums1[k--] = nums2[j--];
+        }
+        return;
+    }
+}
