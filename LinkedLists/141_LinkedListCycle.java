634761e324e5496d6aeeb644c060a32753e6cf9d
Irene1028 <49041696+Irene1028@users.noreply.github.com>
2019-08-09 14:13:29 -0400

    Update 141_LinkedListCycle.java

--- LinkedLists/141_LinkedListCycle.java
+++ LinkedLists/141_LinkedListCycle.java
@@ -27,3 +27,32 @@ public class Solution {
         return false;
     }
 }
+
+// a O(1) space complexity solution
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        // single node or null list
+        if(head == null || head.next == null) return false;
+        
+        // long list
+        ListNode slow = head, fast = head.next;
+        while(slow != fast){
+            if(fast == null || fast.next == null) return false;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+}
