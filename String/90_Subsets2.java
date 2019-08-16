751d2a23600e0b6181ed94eaf717edc125983776
Irene1028 <49041696+Irene1028@users.noreply.github.com>
2019-08-16 16:19:33 -0400

    Update 90_Subsets2.java

--- String/90_Subsets2.java
+++ String/90_Subsets2.java
@@ -27,3 +27,40 @@ class Solution {
         helper(nums, index + 1, result, cur);
     }
 }
+
+/*
+A faster solution from Guanwen
+*/
+
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);  // nlogn
+        List<List<Integer>> result = new ArrayList<>();
+        if(nums == null) return result;
+        List<Integer> cur = new ArrayList<>();
+        
+        helper(nums, 0, result, cur);
+        return result;
+    }
+    
+    private void helper(int[] nums, int index, List<List<Integer>> result, List<Integer> cur){
+        // base case
+        if(index == nums.length){
+            result.add(new ArrayList<>(cur));
+            return;
+        }
+        
+        // add
+        cur.add(nums[index]);
+        helper(nums, index + 1, result, cur);
+        cur.remove(cur.size() - 1);
+        // skip all same numbers
+        while (index + 1 < nums.length && nums[index + 1] == nums[index]) {
+            index++;
+        }
+        // not add
+        helper(nums, index + 1, result, cur);
+    }
+}
+// Time = O(2^n)
+// Extra space = O(n)
