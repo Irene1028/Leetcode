e56a6ba9c0c7ad30060c14e4c9c3e279be3c759e
Irene1028 <49041696+Irene1028@users.noreply.github.com>
2019-08-18 15:09:49 -0400

    Update 19_RemoveFromEnd.java

--- LinkedLists/19_RemoveFromEnd.java
+++ LinkedLists/19_RemoveFromEnd.java
@@ -36,3 +36,70 @@ class Solution {
         return newList;
     }
 }
+
+/* Brute Force, two pass solution */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if(head == null || head.next == null) return null;
+        int len = 0;
+        ListNode pointer = head;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        while(pointer != null){
+            len++;
+            pointer = pointer.next;
+        }
+        pointer = dummy;
+        int count = len - n;
+        System.out.println(count);
+        while(count > 0){
+            pointer = pointer.next;
+            count--;
+        }
+        // System.out.println(count);
+        pointer.next = pointer.next.next;
+        return dummy.next;
+    }
+    
+}
+
+/* Two Pointer Solution fast solution */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if(head == null || head.next == null) return null;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode slow = dummy, fast = dummy;
+        
+        while(n > 0){
+            fast = fast.next;
+            n--;
+        }
+        
+        while(fast.next != null){
+            fast = fast.next;
+            slow = slow.next;
+        }
+        // System.out.println(count);
+        slow.next = slow.next.next;
+        return dummy.next;
+    }
+    
+}
