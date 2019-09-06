9f63532c6f0db978c259b12e7efd12ab68ba418a
Irene1028 <49041696+Irene1028@users.noreply.github.com>
2019-09-05 21:02:20 -0400

    Update 222_CompleteTreeNodes.java

--- BST/222_CompleteTreeNodes.java
+++ BST/222_CompleteTreeNodes.java
@@ -16,3 +16,42 @@ class Solution {
         return leftNodes + rightNodes + 1;
     }
 }
+
+// Time O(n)
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int countNodes(TreeNode root) {
+        if (root == null) return 0;
+        int left_height = 1, right_height = 1;
+        TreeNode cur = root;
+        while (cur.left != null) {
+            left_height++;
+            cur = cur.left;
+        }
+        cur = root;
+        while (cur.right != null) {
+            right_height++;
+            cur = cur.right;
+        }
+        if (left_height > right_height) {
+            int leftNodes = countNodes(root.left);
+            int rightNodes = countNodes(root.right);
+            return leftNodes + rightNodes + 1;
+        }
+        if (left_height == right_height) {
+            return (int)Math.pow(2, left_height) - 1;
+        }
+        return 0;
+    }
+}
+
+// Time O(logn * logn)
