b9d3f69074bf7bea28276928c888587bca9211d7
Irene1028 <49041696+Irene1028@users.noreply.github.com>
2019-09-16 20:56:36 -0400

    Update 221_maxSquare.java

--- DP/221_maxSquare.java
+++ DP/221_maxSquare.java
@@ -5,11 +5,7 @@ class Solution {
         }
         int len = matrix[0].length, height = matrix.length;
         int max = 0;
-        // if (len == 1 || height == 1) {
-        //     max = 1;
-        // }
         int[][] dp = new int[height][len];
-        //int max = Integer.MIN_VALUE;
         // fill the last column
         for (int i = 0; i < height; i++) {
             dp[i][len-1] = Character.getNumericValue(matrix[i][len-1]);
