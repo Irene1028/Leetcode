1e6fcc6913ebeb62c045cd63bf18600b56701e46
Irene1028 <49041696+Irene1028@users.noreply.github.com>
2019-09-28 17:10:28 -0400

    Update 88_MergeSortedArray.java

--- Array/88_MergeSortedArray.java
+++ Array/88_MergeSortedArray.java
@@ -20,3 +20,24 @@ class Solution {
         return;
     }
 }
+
+
+// updated solution. 09/28/19
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        if (n == 0) return;
+        int i = m - 1, j = n - 1, k = nums1.length - 1;
+        while (j >= 0) {
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            }
+            else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+        return;
+    }
+}
