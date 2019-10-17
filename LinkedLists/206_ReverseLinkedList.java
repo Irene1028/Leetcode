568d24eae475ab91026141c90e7ad9afc11d21c2
Irene1028 <49041696+Irene1028@users.noreply.github.com>
2019-10-17 15:33:52 -0400

    Update 206_ReverseLinkedList.java

--- LinkedLists/206_ReverseLinkedList.java
+++ LinkedLists/206_ReverseLinkedList.java
@@ -21,3 +21,59 @@ class Solution {
         return newNode;
     }
 }
+
+/*
+Time = O(n), Space = O(n)
+*/
+
+/************** In Place Solution ***************/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode curNode = head;
+        ListNode prevNode = null;
+        while (curNode != null) {
+            ListNode nextCur = curNode.next;
+            curNode.next = prevNode;
+            prevNode = curNode;
+            curNode = nextCur;
+        }
+        return prevNode;
+    }
+}
+
+/* Time = O(n), Space = O(1). */
+
+/*************** Recursion ****************/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode newHead = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+}
+/* Time = O(n), Space = O(1). */
+
