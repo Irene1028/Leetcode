83be18df7806e30e1b2bb0751be19a29b04c4034
Irene1028 <49041696+Irene1028@users.noreply.github.com>
2019-11-02 17:55:30 -0400

    Update 347_topKFrequent.java

--- 347_topKFrequent.java
+++ 347_topKFrequent.java
@@ -22,3 +22,38 @@ class Solution {
         return ans;
     }
 }
+/*
+Time: O(nlogn)
+Space: O(n)
+*/
+
+class Solution {
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        if (nums.length == 0) {
+            return null;
+        }
+        List<Integer> ans = new ArrayList<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+        PriorityQueue<Integer> heap = new PriorityQueue<Integer>((n1, n2) -> map.get(n1) - map.get(n2));
+        // first, increasing order
+        for (int key : map.keySet()) {
+            heap.add(key);
+            if (heap.size() > k) {
+                heap.poll();
+                // poll out the top/smallest one.
+            }
+        }
+        for (int i = 0; i < k; i++) {
+            ans.add(heap.poll());
+        }
+        Collections.reverse(ans);
+        return ans;
+    }
+}
+/*
+Time: O(nlogk);
+Space: O(n);
+*/
