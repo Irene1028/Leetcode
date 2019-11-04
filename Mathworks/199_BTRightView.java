9ab6b942c36c6aac716ac0128f40ee7a936200cf
Irene1028 <49041696+Irene1028@users.noreply.github.com>
2019-11-04 11:25:54 -0500

    Update 199_BTRightView.java

--- Mathworks/199_BTRightView.java
+++ Mathworks/199_BTRightView.java
@@ -27,6 +27,52 @@ class Solution {
     }
 }
 /*
+DFS + recursion method
 Time O(n)
 Space O(n)
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root == null) return list;
+        Map<Integer, TreeNode> map = new HashMap<>();
+        Queue<Integer> depth = new LinkedList<>();
+        Queue<TreeNode> node = new LinkedList<>();
+        depth.offer(0);
+        node.offer(root);
+        while (!depth.isEmpty()) {
+            int d = depth.poll();
+            TreeNode n = node.poll();
+            map.put(d, n);
+            if(n.left != null) {
+                depth.offer(d + 1);
+                node.offer(n.left);
+            }
+            if (n.right != null) {
+                depth.offer(d + 1);
+                node.offer(n.right);
+            }
+            
+        }
+        
+        for (int i = 0; i < map.size(); i++) {
+            list.add(map.get(i).val);
+        }
+        return list;
+    }
+    
+}
+/*
+BFS
+
 */
