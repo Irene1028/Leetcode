505b0a7d8d011b3a30eabe9c965b7fb3f93a063e
Irene1028 <49041696+Irene1028@users.noreply.github.com>
2019-11-23 21:12:37 -0500

    Update 3_longestWithoutDuplicate.java

--- Amazon/3_longestWithoutDuplicate.java
+++ Amazon/3_longestWithoutDuplicate.java
@@ -23,3 +23,22 @@ class Solution {
 
 // Time is too slow, O(n^2)
 // Space is O(n)
+
+// O(n) Solution
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        if (s.length() == 0 || s.length() == 1) return s.length();
+        Map<String, Integer> map = new HashMap<>();
+        int left = 0, right = 0;
+        int max = 0;
+        for (right = 0; right < s.length(); right++) {
+            String letter = s.substring(right, right + 1);
+            if (map.containsKey(letter)) {
+                left = Math.max(map.get(letter) + 1, left);
+            }            
+            map.put(letter, right);
+            max = Math.max(right - left + 1, max);
+        }
+        return max;
+    }
+}
