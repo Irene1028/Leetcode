73867819acf562aad38b2c6465c1269e6e403c99
Irene1028 <49041696+Irene1028@users.noreply.github.com>
2019-11-25 18:09:21 -0500

    Update 240_SearchMatrix2.java

--- Amazon/240_SearchMatrix2.java
+++ Amazon/240_SearchMatrix2.java
@@ -11,3 +11,21 @@ class Solution {
         return false;
     }
 }
+
+// 2. O(m*n), traverse one by one but break early
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix.length == 0 || matrix[0].length == 0) return false;
+        for (int i = 0; i < matrix.length; i++) {
+            if (matrix[i][0] > target) return false;
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == target) {
+                    return true;
+                } else if (matrix[i][j] > target) {
+                    break;
+                } 
+            }
+        }
+        return false;
+    }
+}
