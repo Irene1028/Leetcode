23c11a8531c5bd09cbf985c2a3d179e3e58cf8db
Irene1028 <49041696+Irene1028@users.noreply.github.com>
2019-11-25 23:21:19 -0500

    Update 240_SearchMatrix2.java

--- Amazon/240_SearchMatrix2.java
+++ Amazon/240_SearchMatrix2.java
@@ -29,3 +29,41 @@ class Solution {
         return false;
     }
 }
+
+// 3. LogN! binary search
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        // 2. use recursion
+        if (matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+        int maxLen = Math.min(matrix.length, matrix[0].length);
+        for (int i = 0; i < maxLen; i++) {
+            //System.out.println(i);
+            boolean rowFound = binarySearch(matrix, target, i, true);
+            boolean colFound = binarySearch(matrix, target, i, false);
+            if (rowFound || colFound) return true;
+        }
+        return false;
+    }
+    private boolean binarySearch(int[][] matrix, int target, int upLeft, boolean searchRow) {
+        int start = upLeft;
+        int end = searchRow? matrix[0].length - 1: matrix.length - 1;
+        while (start <= end) {
+            int mid = (end + start) / 2;
+            
+            if (searchRow) {
+                //System.out.print(mid);
+                if (matrix[upLeft][mid] == target) return true;
+                if (matrix[upLeft][mid] > target) end = mid - 1;
+                if (matrix[upLeft][mid] < target) start = mid + 1;
+            } else {
+                //System.out.println(mid);
+                if (matrix[mid][upLeft] == target) return true;
+                if (matrix[mid][upLeft] > target) end = mid - 1;
+                if (matrix[mid][upLeft] < target) start = mid + 1;
+            }
+        }
+        return false;
+    }
+}
