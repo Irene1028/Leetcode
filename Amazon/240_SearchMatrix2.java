044a6badb1c5c3be53c72b76fa59b6263669e957
Irene1028 <49041696+Irene1028@users.noreply.github.com>
2019-11-26 00:28:01 -0500

    Update 240_SearchMatrix2.java

--- Amazon/240_SearchMatrix2.java
+++ Amazon/240_SearchMatrix2.java
@@ -90,3 +90,17 @@ class Solution {
             || devideMatrix(matrix, target, mid + 1, right, up, row - 1);
     }
 }
+
+// awesome! O(m + n)
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row = matrix.length - 1;
+        int col = 0;
+        while (row >= 0 && col < matrix[0].length) {
+            if (matrix[row][col] == target) return true;
+            if (matrix[row][col] > target) row--;
+            else col++;
+        }
+        return false;
+    }
+}
