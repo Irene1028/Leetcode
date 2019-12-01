662f407b7c8bbe1001116237001739fbb77b3d69
Irene1028 <49041696+Irene1028@users.noreply.github.com>
2019-12-01 16:13:00 -0500

    Update 255_verifyPreorderBST.java

--- Mathworks/255_verifyPreorderBST.java
+++ Mathworks/255_verifyPreorderBST.java
@@ -18,3 +18,33 @@ class Solution {
 
 // Time = O(n), traverse one pass
 // Space = O(n), in case that the array is a desending order
+
+class Solution {
+    public boolean verifyPreorder(int[] preorder) {
+        // stupid method
+        // find left subtree, and see whether all right subtree nodes smaller than cur root.
+        return verify(preorder, 0, preorder.length - 1);
+        
+    }
+    private boolean verify(int[] preorder, int start, int end) {
+        if (start >= end) return true;
+        int left = start + 1;
+        while (left <= end && preorder[left] < preorder[start]) {
+            left++;
+        }
+        if (left > end) {
+            return verify(preorder, start + 1, end);
+        }
+        int right = left;
+        while (right <= end) {
+            if (preorder[right] < preorder[start]) {
+                return false;
+            }
+            right++;
+        }
+        return verify(preorder, start + 1, left - 1) && verify(preorder, left, end);
+    }
+}
+
+// Time = O(n), traverse one pass
+// Space = O(n), in case that the array is a desending order
